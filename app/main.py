3abcfd9451e60982a89f73a8fcbe43302b88e48c
wltwt <wltwt@proton.me>
2025-07-13 13:54:24 +0200

    Continue building dev environment

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from the Pi!"
+    return "Test flask app"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True)
